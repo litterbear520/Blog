e804899e596ff87023b3573bc6c54133c74093b6
littlebear <970219247@qq.com>
2025-07-04 12:54:08 +0800

    更新README.md，添加网站介绍、安装、开发、构建和部署的详细说明

---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+
+import Layout from '@theme/Layout';
+
+
+
+
+function HomepageHeader() {
+
+  return (
+    <header>
+      <div className="container">
+      little bear
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+
+  return (
+    <Layout>
+      <HomepageHeader />
+      <main>
+
+      </main>
+    </Layout>
+  );
+}
